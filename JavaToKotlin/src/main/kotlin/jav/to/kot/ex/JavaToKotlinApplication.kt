dbbbc6b2061690ac517ca3fa6cda92c058d3e866
BAEKJungHo <designjava@naver.com>
2023-01-01 00:25:23 +0900

    Create Project

---
+++ JavaToKotlin/src/main/kotlin/jav/to/kot/ex/JavaToKotlinApplication.kt
@@ -0,0 +1,11 @@
+package jav.to.kot.ex
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JavaToKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<JavaToKotlinApplication>(*args)
+}
---
+++ JavaToKotlin/src/test/kotlin/jav/to/kot/ex/JavaToKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package jav.to.kot.ex
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JavaToKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
